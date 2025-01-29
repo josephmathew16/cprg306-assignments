c6e0925fc7f2ac1c9fefc0a41f6d1444bf083bf7
Joseph Mathew <Joseph.Mathew@edu.sait.ca>
2025-01-29 10:34:49 -0700

    All Updates Done

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Joseph Mathew</p>
+            <p>
+             <Link href="https://github.com/josephmathew16"> Link to Repo </Link>   
+            </p>    
+        </div>
+    )
+}
